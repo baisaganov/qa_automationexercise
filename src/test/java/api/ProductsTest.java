b77052d83dec0486781078261e1eb876938c1c98
Alisher Baisaganov <baisaganov99@gmail.com>
2023-10-24 23:29:07 +0600

    API Tests

---
+++ src/test/java/api/ProductsTest.java
@@ -0,0 +1,106 @@
+package api;
+
+import io.restassured.RestAssured;
+import io.restassured.filter.Filter;
+import io.restassured.http.ContentType;
+import io.restassured.internal.RestAssuredResponseOptionsImpl;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.core.Is.is;
+
+public class ProductsTest {
+
+    static final Filter FORCE_JSON_RESPONSE_BODY = (reqSpec, respSpec, ctx) -> {
+        Response response = ctx.next(reqSpec, respSpec);
+        ((RestAssuredResponseOptionsImpl<?>) response).setContentType("application/json");
+        return response;
+    };
+
+    @Test
+    @DisplayName("API 1: Get All Products List")
+    @Tag("products_api")
+    void getAllProductsTest(){
+        given()
+                .filters(FORCE_JSON_RESPONSE_BODY)
+                .get("https://automationexercise.com/api/productsList")
+                .then()
+                .statusCode(200)
+                .body("products.size()", greaterThan(30));
+    }
+
+    @Test
+    @DisplayName("API 2: POST To All Products List")
+    @Tag("products_api")
+    void postAllProductsTest(){
+        given().urlEncodingEnabled(true)
+                .filters(FORCE_JSON_RESPONSE_BODY)
+                .when()
+                .post("https://automationexercise.com/api/productsList")
+                .then()
+                .body("responseCode", is(405))
+                .and()
+                .body("message", equalTo("This request method is not supported."));
+    }
+
+    @Test
+    @DisplayName("API 3: Get All Brands List")
+    @Tag("products_api")
+    void getAllBrandsListTest(){
+        given().urlEncodingEnabled(true)
+                .filters(FORCE_JSON_RESPONSE_BODY)
+                .get("https://automationexercise.com/api/brandsList")
+                .then()
+                .body("responseCode", is(200))
+                .and()
+                .body("brands.size()", greaterThan(30));
+    }
+
+    @Test
+    @DisplayName("API 4: PUT To All Brands List")
+    @Tag("products_api")
+    void putToAllBrandsListTest(){
+        given().urlEncodingEnabled(true)
+                .filters(FORCE_JSON_RESPONSE_BODY)
+                .put("https://automationexercise.com/api/brandsList")
+                .then()
+                .body("responseCode", is(405))
+                .and()
+                .body("message", is("This request method is not supported."));
+    }
+
+    @Test
+    @DisplayName("API 5: POST To Search Product")
+    @Tag("products_api")
+    void postToSearchProductTest(){
+        given().urlEncodingEnabled(true)
+                .filters(FORCE_JSON_RESPONSE_BODY)
+                .accept(ContentType.JSON)
+                .contentType(ContentType.MULTIPART)
+                .multiPart("search_product", "top")
+                .when()
+                .post("https://automationexercise.com/api/searchProduct")
+                .then().log().all()
+                .body("responseCode", is(200))
+                .and()
+                .body("products.size()", greaterThan(10));
+    }
+
+    @Test
+    @DisplayName("API 6: POST To Search Product without search_product parameter")
+    @Tag("products_api")
+    void postErrorToSearchProductTest(){
+        given().urlEncodingEnabled(true)
+                .filters(FORCE_JSON_RESPONSE_BODY)
+                .accept(ContentType.JSON)
+                .contentType(ContentType.MULTIPART)
+                .when()
+                .post("https://automationexercise.com/api/searchProduct")
+                .then().log().all()
+                .body("responseCode", is(400));
+    }
+}
---
+++ src/test/java/api/UserTest.java
@@ -0,0 +1,145 @@
+package api;
+
+import io.restassured.filter.Filter;
+import io.restassured.http.ContentType;
+import io.restassured.internal.RestAssuredResponseOptionsImpl;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.Is.is;
+
+public class UserTest {
+
+    private String user_email = (Math.random()*Math.random())+"@dasdaz.das";
+
+    static final Filter FORCE_JSON_RESPONSE_BODY = (reqSpec, respSpec, ctx) -> {
+        Response response = ctx.next(reqSpec, respSpec);
+        ((RestAssuredResponseOptionsImpl<?>) response).setContentType("application/json");
+        return response;
+    };
+
+    @Test
+    @DisplayName("API 7: POST To Verify Login with valid details")
+    @Tag("user_api")
+    void postLoginTest(){
+        given().urlEncodingEnabled(true)
+                .filters(FORCE_JSON_RESPONSE_BODY)
+                .accept(ContentType.JSON)
+                .contentType(ContentType.MULTIPART)
+                .multiPart("email", "test3@gamail.com")
+                .multiPart("password", "Password1")
+                .when()
+                .post("https://automationexercise.com/api/verifyLogin")
+                .then().log().all()
+                .body("responseCode", is(200))
+                .and()
+                .body("message", is("User exists!"));
+    }
+
+    @Test
+    @DisplayName("API 8: POST To Verify Login without email parameter")
+    @Tag("user_api")
+    void postErrorWithoutEmailLoginTest(){
+        given().urlEncodingEnabled(true)
+                .filters(FORCE_JSON_RESPONSE_BODY)
+                .accept(ContentType.JSON)
+                .contentType(ContentType.MULTIPART)
+                .multiPart("password", "Password1")
+                .when()
+                .post("https://automationexercise.com/api/verifyLogin")
+                .then().log().all()
+                .body("responseCode", is(400))
+                .and()
+                .body("message", is("Bad request, email or password parameter is missing in POST request."));
+    }
+
+    @Test
+    @DisplayName("API 9: DELETE To Verify Login")
+    @Tag("user_api")
+    void deleteToVerifyLoginTest(){
+        given()
+                .urlEncodingEnabled(true)
+                .filters(FORCE_JSON_RESPONSE_BODY)
+                .when()
+                .delete("https://automationexercise.com/api/verifyLogin")
+                .then().log().all()
+                .body("responseCode", is(405))
+                .and()
+                .body("message", is("This request method is not supported."));
+    }
+
+    @Test
+    @DisplayName("API 10: POST To Verify Login with invalid details")
+    @Tag("user_api")
+    void postErrorWithInvalidValuesToVerifyLoginTest(){
+        given()
+                .urlEncodingEnabled(true)
+                .filters(FORCE_JSON_RESPONSE_BODY)
+                .multiPart("email", "123")
+                .multiPart("password", "123")
+                .when()
+                .post("https://automationexercise.com/api/verifyLogin")
+                .then().log().all()
+                .body("responseCode", is(404))
+                .and()
+                .body("message", is("User not found!"));
+    }
+
+    @Test
+    @DisplayName("API 11: POST To Create/Register User Account")
+    @Tag("user_api")
+    void postCreateUserTest(){
+        given()
+                .urlEncodingEnabled(true)
+                .filters(FORCE_JSON_RESPONSE_BODY)
+                .multiPart("email", user_email)
+                .multiPart("password", "123")
+                .multiPart("name", "123")
+                .multiPart("title", "Mr")
+                .multiPart("birth_date", "11")
+                .multiPart("birth_month", "11")
+                .multiPart("birth_year", "1999")
+                .multiPart("firstname", "123")
+                .multiPart("lastname", "123")
+                .multiPart("company", "123")
+                .multiPart("address1", "123")
+                .multiPart("address2", "123")
+                .multiPart("country", "India")
+                .multiPart("zipcode", "123")
+                .multiPart("state", "123")
+                .multiPart("city", "123")
+                .multiPart("mobile_number", "123")
+                .when()
+                .post("https://automationexercise.com/api/createAccount")
+                .then().log().all()
+                .body("responseCode", is(201))
+                .and()
+                .body("message", is("User created!"));
+    }
+
+
+    @Test
+    @DisplayName("API 12: DELETE METHOD To Delete User Account")
+    @Tag("user_api")
+    void deleteUserAccountTest(){
+        given()
+                .urlEncodingEnabled(true)
+                .filters(FORCE_JSON_RESPONSE_BODY)
+                .multiPart("email", user_email)
+                .multiPart("password", "123")
+                .when()
+                .delete("https://automationexercise.com/api/deleteAccount")
+                .then().log().all()
+                .body("responseCode", is(200))
+                .and()
+                .body("message", is("Account deleted!"));
+    }
+
+
+
+
+
+}
